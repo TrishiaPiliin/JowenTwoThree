f8d7fdd12fd36c8bf96a42a24fa3524b9d5bd13f
TrishiaPiliin <trishiamaepiliin28@gmail.com>
2026-07-21 10:30:25 +0800

    Week 5 - Day 1 POS Processing (#37)
    * Week 1 - Day 1
    
    * Week 1 - Day 1
    
    * added pos
    
    * Desin POS Layout
    
    * fix remove the node modules and fix some file structure
    
    * modifying some files
    
    * fix project structure
    
    * working some errors
    
    * fixing json files
    
    * Week 1 - POS System
    
    * splitting ProductCard and ProductPage
    
    * Design Order Summary UI
    
    * splitting components
    
    * Fix bugs in the Pages and the main.jsx
    
    * Updated Design Order Summary
    
    * Discount and Total Amount
    
    * adding discount deduction test
    
    * Design Receipt
    
    * Adding Integration
    
    * Fixed error
    
    * Fixed errors
    
    * Week 3 Day 1 - Fixing Code for Backend
    
    * Fixed some errors
    
    * Login Authentication
    
    * Modifying authService.test.js
    
    * Ob1W3D1
    
    * Fixed Errors for back-end
    
    * Week 3 Day 1 - Authentication and Testing
    
    * Updating quantity and discount
    
    * Updating
    
    * Updating
    
    * Fix import path for mock inventory data
    
    * Adding warning messages
    
    * Added fix
    
    * Refactor transactionService tests to improve structure and add validation for transaction records
    
    * Week 5 - Day 1

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const alertsRouter = require("./routes/alertsRoute")
 const salesRouter = require("./routes/salesRoute")
 const salesTrendRouter = require("./routes/salesTrendRoute")
 const customerTrafficRouter = require("./routes/customerTrafficRoute")
+const transactionRouter = require("./routes/transactionRoute") // NEW
 
 const app = express()
 
@@ -21,5 +22,6 @@ app.use("/api/inventory/alerts", alertsRouter)
 app.use("/api/sales", salesRouter)
 app.use("/api/sales/trend", salesTrendRouter)
 app.use("/api/customer-traffic", customerTrafficRouter)
+app.use("/api/transactions", transactionRouter) // NEW
 
 module.exports = app
